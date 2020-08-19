0bf1063e64ada30f302d61f222a8baef24e18bab
Taylor Allen <tjaroguetech@gmail.com>
2020-08-19 20:17:51 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  n = 2
+  while n < num
+    return false if num % n == 0 
+      n += 1
+    end
+    if num <= 1 
+      return false 
+    end
+  return true
+end 
